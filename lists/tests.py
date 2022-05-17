f8155b360de94908e42666f98d699395310c5bb4
xiewei <1013615300@qq.com>
2022-05-17 14:56:50 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,24 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page #(2)
+from lists.models import Item
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item=Item()
+		first_item.text='The first (ever) list item'
+		first_item.save()
 
+		second_item=Item()
+		second_item.text='Item the second'
+		second_item.save()
+
+		save_items = Item.objects.all()
+		self.assertEqual(save_items.count(),2)
+
+		first_save_item = save_items[0]
+		second_save_item = save_items[1]
+		self.assertEqual(first_save_item.text,'The first (ever) list item')
+		self.assertEqual(second_save_item,'Item the second')
 class HomePageTest(TestCase):
 	def test_root_url_resolve_to_home_page_view(self):
 		found  =resolve('/') #(1)
@@ -16,6 +33,11 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text':'A new list item'})
+
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text,'A new list item')
+
 		self.assertIn('A new list item',response.content.decode())
 		self.assertTemplateUsed(response,'home.html')
 
