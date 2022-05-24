3a8c550da9b3cc867eecb8fcc3f8e6c6b4394301
xiewei <1013615300@qq.com>
2022-05-24 13:56:37 +0800

    Add a new user in system

--- lists/tests.py
+++ lists/tests.py
@@ -31,6 +31,11 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response,'home.html')
 
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code,302)
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+		
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 
